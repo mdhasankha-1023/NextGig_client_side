20573d16b450f41ce000a51ac3ce2c652650430f
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-06-03 11:40:35 +0600

    Add: DaisyUi components freamwork added

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,6 +1,14 @@
 
 export default function MainLayout() {
   return (
-    <div>MainLayoute</div>
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import MainLayout from './Layouts/MainLayout/MainLayout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MainLayout/>
   </React.StrictMode>,
 )
